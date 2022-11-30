f674b23b4bf24f538794ed22e51ae554cf2da4f6
Ashe <yurirussu.mail@gmail.com>
2022-11-30 16:07:31 +0300

    prod env test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,11 +8,25 @@
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from '@/components/HelloWorld.vue';
+import { Loader } from "@/utils";
+import axios from "axios";
+import {ApiService} from "@/api/services";
 
 @Options({
   components: {
     HelloWorld,
   },
 })
-export default class HomeView extends Vue {}
+export default class HomeView extends Vue {
+  created() {
+    Loader.Use(async () => {
+      //return new Promise(r => setTimeout(r, 3000));
+      return await axios.get(ApiService.CreateApiRequestUrl({
+        path: ["gkassa", "services"]
+      }).Url);
+    }).then(response => {
+      console.log(response);
+    });
+  }
+}
 </script>
